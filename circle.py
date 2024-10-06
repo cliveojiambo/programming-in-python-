e15d151696695ecf54367125cc93986ef0a3c123
cliveojiambo <158243976+cliveojiambo@users.noreply.github.com>
2024-10-06 09:29:52 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -3,3 +3,13 @@ import math
 def area_of_circle(radius):
     return math.pi * radius ** 2
 
+def main():
+  
+    radius = float(input("Enter the radius of the circle: "))
+ 
+    area = area_of_circle(radius)
+
+    print(f"The area of the circle is: {area:.2f}")
+
+if __name__ == "__main__":
+    main()
