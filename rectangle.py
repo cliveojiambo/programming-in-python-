cec7b6c40308605667b2ca2c5b4c3dc6ab081e76
cliveojiambo <158243976+cliveojiambo@users.noreply.github.com>
2024-10-06 09:27:19 +0300

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,3 +1,15 @@
 def area_of_rectangle(length, width):
     return length * width
 
+
+def main():
+ 
+    length = float(input("Enter the length of the rectangle: "))
+    width = float(input("Enter the width of the rectangle: "))
+
+    area = area_of_rectangle(length, width)
+ 
+    print(f"The area of the rectangle is: {area:.2f}")
+    
+if __name__ == "__main__":
+    main()
